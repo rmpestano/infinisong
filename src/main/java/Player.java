1853617336f1c1a197d188c597f8d39ce8dfef68
rmpestano <bigboss666>
2015-09-07 20:41:40 -0300

    second commit

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,15 +1,8 @@
 import javazoom.jl.decoder.Bitstream;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.advanced.AdvancedPlayer;
-import javazoom.jl.player.advanced.PlaybackEvent;
-import javazoom.jl.player.advanced.PlaybackListener;
 
-import javax.media.Manager;
-import javax.media.NoProcessorException;
-import javax.media.bean.playerbean.MediaPlayer;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * Created by pestano on 06/09/15.
@@ -18,8 +11,7 @@ public class Player {
 
 
 
-    public static void main(String[] args) throws IOException, NoProcessorException, JavaLayerException {
-        javax.media.Player p = Manager.createProcessor(new URL(""));
+    public static void main(String[] args) throws IOException, JavaLayerException {
         AdvancedPlayer player = new AdvancedPlayer(null);
 
         Bitstream bitstream;
